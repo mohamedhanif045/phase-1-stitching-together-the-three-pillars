f28f9b077399a8edbdebd8dd1de7d2f67a242f7c
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-05-24 08:22:03 +0300

    stiching together assignment

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
 /* This is a file inserted to keep the learn IDE browser happy */
 const testVar = {}
+
+const articleHearts = document.querySelectorAll(".like-glyph");
+
+// Step 2: Mock server communication
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+// Step 3: Add event listeners to each heart
+for (const glyph of articleHearts) {
+    glyph.addEventListener("click", handleLike);
+  }
+  
+  function handleLike(e) {
+    const heart = e.target;
+    mimicServerCall()
+      .then(function(serverMessage){
+        heart.innerText = heart.innerText === "♡" ? "♥" : "♡";
+        heart.classList.toggle("activated-heart");
+      })
+      .catch(function(error) {
+        alert("Something went wrong!");
+      });
+  }
+  
